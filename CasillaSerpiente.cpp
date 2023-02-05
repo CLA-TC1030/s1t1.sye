4440c500759bec7dfeccde069ae88ea96d1cc2d4
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2023-02-04 22:10:29 -0600

    Update CasillaSerpiente.cpp

--- CasillaSerpiente.cpp
+++ CasillaSerpiente.cpp
@@ -2,13 +2,13 @@
 #include "CCasilla.hpp"
 #include "Ctesconf.hpp"
 
-//Construye una Casilla Serpiente de numero (1-SER_PREMIO CASTIGO), con premio-castigo (SER_PREMIO_CASTIGO) y de tipo "s"
+//Construye una Casilla Serpiente de numero (1-SER_PREMIO CASTIGO), con premio-castigo (SER_PREMIO_CASTIGO) y de tipo "S"
 CasillaSerpiente::CasillaSerpiente()
 {
     
 }
 
-//Construye una Casilla Serpiente de numero (numero), con premio-castigo (SER_PREMIO_CASTIGO) y de tipo "s"
+//Construye una Casilla Serpiente de numero (numero), con premio-castigo (SER_PREMIO_CASTIGO) y de tipo "S"
 CasillaSerpiente::CasillaSerpiente(int numero):CCasilla{numero, "S"}
 {
     
